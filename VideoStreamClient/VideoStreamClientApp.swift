cba0776dc0f8ceb88c2bae9e941bf6873f1c0633
Arthur Zhang <azhangvo@gmail.com>
2023-02-04 18:37:57 -0600

    Initial Commit

---
+++ VideoStreamClient/VideoStreamClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoStreamClientApp.swift
+//  VideoStreamClient
+//
+//  Created by Arthur Zhang on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoStreamClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
