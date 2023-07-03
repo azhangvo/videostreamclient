9e59833976a079f49cc1f1a59fe978b86ff0c713
Arthur Zhang <azhangvo@gmail.com>
2023-07-03 19:52:18 -0400

    basic UI

--- VideoStreamClient/ContentView.swift
+++ VideoStreamClient/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                VStack(alignment: .leading) {
+                    NavigationLink(destination: Text("Another view")) {
+                        Text("Settings")
+                            .foregroundColor(Color.blue)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        }
+        
+        
     }
 }
 
